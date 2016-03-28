b1c5be7c2bb02f071a610950864d2f12d8212118
mazdik <remappp@gmail.com>
2016-03-28 22:04:05 +0500

    email validator

--- app/main.ts
+++ app/main.ts
@@ -1,4 +1,9 @@
 import {bootstrap}    from 'angular2/platform/browser'
 import {AppComponent} from './app.component'
+import {enableProdMode} from 'angular2/core';
+
+if (window['IS_PROD'] === 'true') {
+  enableProdMode();
+}
 
 bootstrap(AppComponent);
--- app/signup.component.ts
+++ app/signup.component.ts
@@ -1,6 +1,7 @@
 import {Component}  from 'angular2/core';
 import {FORM_DIRECTIVES, ControlGroup, FormBuilder, Validators} from 'angular2/common';
 import {JSONP_PROVIDERS, Http, HTTP_PROVIDERS, Headers, RequestOptions}  from 'angular2/http';
+import {validateEmail} from './web.util';
 
 class User {
     public name: string;
@@ -21,7 +22,7 @@ export class SignupComponent {
   constructor (private _formBuilder: FormBuilder, private http: Http) {
     this.signupForm = this._formBuilder.group({
       name: ["", Validators.compose([Validators.required, Validators.minLength(2), Validators.maxLength(45)])],
-      email: ["", Validators.required],
+      email: ["", Validators.compose([Validators.required, validateEmail])],
       passwordRetry: this._formBuilder.group({
         password: ["", Validators.required],
         passwordConfirmation: ["", Validators.required]
@@ -33,13 +34,13 @@ export class SignupComponent {
     let headers = new Headers({ 'Content-Type': 'application/x-www-form-urlencoded' });
     let options = new RequestOptions({ headers: headers });
     return this.http
-      .post('/client/index.php/site/signupjson', JSON.stringify(credentials), options)
+      .post('http://host3/client/index.php/site/signupjson', JSON.stringify(credentials), options)
       .map(res => res.json())
       .map((res) => {
         if (res.success) {
         	console.log('success');
         }
-
+        console.log(res);
         return res.success;
       });
   }
@@ -49,11 +50,15 @@ export class SignupComponent {
   submitted = false;
 
   onSubmit(credentials) {
-  	this.submitted = true;
-  	this.signup(credentials);
-    this.model = new User();
-    this.active = false;
-    setTimeout(()=> this.active=true, 0);
+    this.signup(credentials).subscribe((result) => {
+      if (result) {
+        //this._router.navigate(['List']);
+      }
+    });
+    // this.submitted = true;
+    // this.model = new User();
+    // this.active = false;
+    // setTimeout(()=> this.active=true, 0);
   }
 
 }
