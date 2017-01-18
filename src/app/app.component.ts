7fcccb9e4f24da718af466e21793181edaa117f6
mazdik <remappp@gmail.com>
2017-01-18 22:35:53 +0500

    Commit description

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
-import {Component, Inject} from '@angular/core';
+import {Component, Inject, OnInit, ViewEncapsulation} from '@angular/core';
 import {AppState} from './app.service';
 import {Config, APP_CONFIG} from './app.config';
 
 @Component({
   selector: 'app',
-  templateUrl: './app.component.html'
+  templateUrl: './app.component.html',
+  encapsulation: ViewEncapsulation.None,
+  styleUrls: ['./app.component.css'],
 })
 
 export class AppComponent {
