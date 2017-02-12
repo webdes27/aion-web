ab9b0ac0fd560be1bc4314ccfb4e36830739d07a
mazdik <remappp@gmail.com>
2017-02-12 15:55:48 +0500

    Commit description

--- src/app/shared/crud-table/crud-table-module.ts
+++ src/app/shared/crud-table/crud-table-module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 
-import { PaginationComponent } from './pagination.component';
-import { ModalComponent } from './modal.component';
+import { PaginationComponent } from './pagination/pagination.component';
+import { ModalComponent } from './modal/modal.component';
+import { HeaderComponent } from './header/header.component';
 import { CrudTableComponent } from './crud-table.component';
-import { CrudService } from './crud.service';
+import { CrudService } from './services/crud.service';
 import { SelectComponent, SelectSearchFilter } from './select/select.component';
 
 @NgModule({
@@ -13,7 +14,14 @@ import { SelectComponent, SelectSearchFilter } from './select/select.component';
   	CommonModule,
     FormsModule
   ],
-  declarations: [CrudTableComponent, PaginationComponent, ModalComponent, SelectComponent, SelectSearchFilter],
+  declarations: [
+	CrudTableComponent, 
+	PaginationComponent, 
+	ModalComponent, 
+	HeaderComponent, 
+	SelectComponent, 
+	SelectSearchFilter
+  ],
   exports: [CrudTableComponent],
   providers: [CrudService]
 })
